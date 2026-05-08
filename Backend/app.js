59e974bdf2413a46f6aee44c2d8179b6363ee052
vanshikapandit-cyber <vanshikapandit@globussoft.in>
2026-05-08 18:23:26 +0530

    addchanges

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>
-{
+app.get('/', (req, res) => {
     res.send("hello from client side ");
 });
-app.listen(3000, ()=>
-{
+app.listen(3000, () => {
     console.log("Server is running at 3000 port 😊");
 });
 console.log("Hi! there");
-
-
-
 
 
--- Frontend/index.php
+++ Frontend/index.php
@@ -1,4 +1,18 @@
-<?php 
-echo "hiii";
+<!DOCTYPE html>
+<html lang="en">
 
-?> 
+<head>
+    <meta charset="UTF-8">
+    <title>My Protected App</title>
+    <!-- Linking your CSS file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="card">
+        <h1>Welcome to Jenkins Dashboard</h1>
+        <p>Thiss is a automation for building, testing and deploying </p>
+        <p>Current Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+    </div>
+</body>
+</html>

